b411a2acc2f4a829c3ef7a140411b64aabbe58e6
ben thacker <benthac@gmail.com>
2021-05-31 17:20:59 -0400

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+var currentDayEl = new Date();
+var date = currentDayEl.getFullYear()+'-'+
+(currentDayEl.getMonth()+1)+'-'+currentDayEl.getDate();
+document.getElementById("currentDay").value = date;
+
+var currentTimeEl = new Date
+var time = currentTimeEl.getHours() + ":" +
+currentTimeEl.getMinutes() + ":" +
+currentTimeEl.getSeconds();
+document.getElementById("currentTime").value = time;
